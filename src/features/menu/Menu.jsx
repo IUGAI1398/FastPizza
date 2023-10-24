cc6969c54300fde114e6234c2c41703b9b9e9070
IUGAI <iugaialeksandrr@gmail.com>
2023-10-24 17:49:36 +0900

    styled a MenuClass

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,7 +4,7 @@ import {getMenu, getOrder} from '../../service/apiRestaurant'
 
 function Menu() {
   const menu = useLoaderData()
-  return <ul>
+  return <ul className='divide-y divide-stone-200 px-2 space-y-2'>
     {menu.map(pizza => <MenuItem pizza={pizza} key={pizza.id}/>)}
   </ul>
 }
